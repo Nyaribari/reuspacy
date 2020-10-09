19cdab7a24ee97df1924d887516851877c52b3aa
Nyaribari Reuben <anyaribari@gmail.com>
2020-10-09 15:04:49 +0300

    progress

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 name='reuspacy',
 version='0.0.1',
-description = 'The package is a simple spacy entity ruler',
+description = 'The package is a simple spacy entity ruler.',
 url = 'git@github.com:Nyaribari/reuspacy-sdk-package.git',
 author='Nyarbari Reuben',
 author_email='anyaribari@gmail.com',

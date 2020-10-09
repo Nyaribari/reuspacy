352d039d7e9e8dbdb56294cdc6f1d357d6f0b3e4
Nyaribari Reuben <anyaribari@gmail.com>
2020-10-09 13:44:03 +0300

    first commit. model creates ruler entity and identifies label LOC in texts

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+name='reuspacy',
+version='0.0.0.1'
+description = 'My first spacy tester package',
+url = 'git@github.com:Nyaribari/reuspacy-sdk-package.git',
+author='Nyarbari Reuben',
+author_email='anyaribari@gmail.com',
+licence='unlicense',
+packages=[
+'reuspacy'
+],
+zip_safe= False
+)

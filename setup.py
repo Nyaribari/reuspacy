74c4fdaa9efa189c4fbd4f64bbc6fc8dfc202fe9
Nyaribari Reuben <anyaribari@gmail.com>
2020-10-10 11:22:58 +0300

    we up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 name='reuspacy',
 version='0.0.1',
 description = 'The package is a simple spacy entity ruler.',
-url = 'ssh://git@github.com/Nyaribari/reuspacy-sdk-package.git',
+url = 'git@github.com:Nyaribari/reuspacy-sdk-package.git',
 author='Nyarbari Reuben',
 author_email='anyaribari@gmail.com',
 licence='unlicense',
